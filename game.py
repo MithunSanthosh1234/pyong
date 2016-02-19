5fdfe1de699c80f4cdaadf889872d4767242d460
Randy Gingeleski <rgingeleski@aol.com>
2016-02-19 17:54:09 -0500

    add ball and paddle

--- game.py
+++ game.py
@@ -24,6 +24,33 @@ class GameObject(object):
     def delete(self):
         self.canvas.delete(self.item)
 
+class Ball(GameObject):
+    def __init__(self, canvas, x, y):
+        self.radius = 10
+        self.direction = [1, -1]
+        self.speed = 10
+        item = canvas.create_oval(x-self.radius, y-self.radius, x+self.radius, y+self.radius, fill='#000000')
+        super(Ball, self).__init__(canvas, item)
+
+class Paddle(GameObject):
+    def __init__(self, canvas, x, y):
+        self.width = 80
+        self.height = 10
+        self.ball = None
+        item = canvas.create_rectangle(x - self.width / 2, y - self.height / 2, x + self.width / 2, y + self.height / 2, fill='#000000')
+        super(Paddle, self).__init__(canvas, item)
+
+    def set_ball(self, ball):
+        self.ball = ball
+
+    def move(self, offset):
+        coords = self.get_position()
+        width = self.canvas.winfo_width()
+        if coords[0] + offset >= 0 and coords[2] + offset <= width:
+            super(Paddle, self).move(offset, 0)
+            if self.ball is not None:
+                self.ball.move(offset, 0)
+
 if __name__ == '__main__':
     root = tk.Tk()
     root.title('Pyong')
